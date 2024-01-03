f1241440cf8e12d043578d1c6c0726ede9c31102
Aka5hChandel <15chandel2000@gmail.com>
2024-01-03 18:47:14 +0530

    First

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline text-red-600">
+          Todo with Redux
+        </h1>
+        <AddTodo />
+        <Todo />
+      </div>
+    </>
+  );
+}
+
+export default App;
